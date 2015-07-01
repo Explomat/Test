ff85ab171d5b52ea17598b050c6d5c3ce82c5c7d
Explomat <cabehok@inbox.ru>
2015-07-01 21:02:37 +0300

    Added form to file

--- js/jsx/Components/ImageSelect.js
+++ js/jsx/Components/ImageSelect.js
@@ -2,52 +2,53 @@ var React = require('React');
 var QuestionActions = require('../../Controllers/startController/actions/QuestionActions');
 
 var ImageSelect = React.createClass({displayName: "ImageSelect",
+
+	handleSubmitFile: function(e) {
+		console.log(e.target);
+	},
+
+	onLoadFrame: function(e){
+		console.log(e.target);
+	},
 		
 	handleChange:function(e) {
-		console.log(e.target.value);
-		if (this.props.changeImg)
-			this.props.changeImg({name: e.target.value});
-		//this.setState({name:e.target.value});
+		e.target.parentNode.submit();
+		//if (this.props.changeImg)
+		//	this.props.changeImg({name: img.value});
 		//React.findDOMNode(this.refs.form).submit();
 	},
 
-	handleSubmit:function(e) {
-		e.preventDefault();
-		console.log('submit');
-		return;
-	},
-
 	handleRemove: function(e) {
-		//document.getElementById('inputFile').value = '';
-		React.findDOMNode(this.refs.test).value = '';
+		e.target.value = '';
 		if (this.props.changeImg)
 			this.props.changeImg(null);
-		//this.setState({name:''});
 	},
 
 	render:function(){
 		var imgName = this.props.img ? this.props.img.name : '';
 		var isDisplayIcon = { display: (!this.props.img || this.props.img.name.trim() == "") ? "none" : "inline-block" }
 		return (
-			React.createElement("form", {ecntype: "multipart/form-data", method: "POST", onSubmit: this.handleSubmit}, 
-				React.createElement("div", {className: "input-group"}, 
-				   	React.createElement("div", {tabIndex: "-1", className: "form-control file-caption  kv-fileinput-caption"}, 
-				   		React.createElement("span", {title: imgName, className: "file-caption-ellipsis"}, "…"), 
-				   		React.createElement("div", {title: imgName, className: "file-caption-name", style: isDisplayIcon}, 
-				   			React.createElement("span", {className: "glyphicon glyphicon-file kv-caption-icon"}, " ", imgName)
-				   		)
-					), 
-				   	React.createElement("div", {className: "input-group-btn"}, 
-				    	React.createElement("button", {type: "button", title: "Clear selected files", className: "btn btn-default fileinput-remove fileinput-remove-button", style: isDisplayIcon, onClick: this.handleRemove}, 
-				    		React.createElement("i", {className: "glyphicon glyphicon-trash"})
-				    	), 
-				       	React.createElement("div", {style: {"overflow": "hidden"}, className: "btn btn-primary btn-file"}, 
-				       		React.createElement("i", {className: "glyphicon glyphicon-picture"}), 
-				       		React.createElement("span", null, "   … "), 
-				       		React.createElement("input", {ref: "test", accept: "image/*", className: "file", type: "file", onChange: this.handleChange, title: "Добавить изображение"})
-				       	)
-				   	)
-				)
+			React.createElement("div", {className: "input-group"}, 
+			   	React.createElement("div", {tabIndex: "-1", className: "form-control file-caption  kv-fileinput-caption"}, 
+			   		React.createElement("span", {title: imgName, className: "file-caption-ellipsis"}, "…"), 
+			   		React.createElement("div", {title: imgName, className: "file-caption-name", style: isDisplayIcon}, 
+			   			React.createElement("span", {className: "glyphicon glyphicon-file kv-caption-icon"}, " ", imgName)
+			   		)
+				), 
+			   	React.createElement("div", {className: "input-group-btn"}, 
+			    	React.createElement("button", {type: "button", title: "Clear selected files", className: "btn btn-default fileinput-remove fileinput-remove-button", style: isDisplayIcon, onClick: this.handleRemove}, 
+			    		React.createElement("i", {className: "glyphicon glyphicon-trash"})
+			    	), 
+			       	React.createElement("div", {style: {"overflow": "hidden"}, className: "btn btn-primary btn-file"}, 
+			       		React.createElement("i", {className: "glyphicon glyphicon-picture"}), 
+			       		React.createElement("span", null, "   … "), 
+
+			       		React.createElement("form", {action: "/async-upload", method: "post", enctype: "multipart/form-data", target: "iframe-name", onSubmit: this.handleSubmitFile}, 
+			       			React.createElement("input", {accept: "image/*", name: "file_name", className: "file", type: "file", onChange: this.handleChange, title: "Добавить изображение"})
+						), 
+						React.createElement("iframe", {name: "iframe-name", src: "#", style: {"display":"none"}, onLoad: this.onLoadFrame})
+			       	)
+			   	)
 			)
 		);	
 	}
--- js/jsx/Components/ImageSelect.jsx
+++ js/jsx/Components/ImageSelect.jsx
@@ -2,53 +2,54 @@ var React = require('React');
 var QuestionActions = require('../../Controllers/startController/actions/QuestionActions');
 
 var ImageSelect = React.createClass({
+
+	handleSubmitFile: function(e) {
+		console.log(e.target);
+	},
+
+	onLoadFrame: function(e){
+		console.log(e.target);
+	},
 		
 	handleChange:function(e) {
-		console.log(e.target.value);
-		if (this.props.changeImg)
-			this.props.changeImg({name: e.target.value});
-		//this.setState({name:e.target.value});
+		e.target.parentNode.submit();
+		//if (this.props.changeImg)
+		//	this.props.changeImg({name: img.value});
 		//React.findDOMNode(this.refs.form).submit();
 	},
 
-	handleSubmit:function(e) {
-		e.preventDefault();
-		console.log('submit');
-		return;
-	},
-
 	handleRemove: function(e) {
-		//document.getElementById('inputFile').value = '';
-		React.findDOMNode(this.refs.test).value = '';
+		e.target.value = '';
 		if (this.props.changeImg)
 			this.props.changeImg(null);
-		//this.setState({name:''});
 	},
 
 	render:function(){
 		var imgName = this.props.img ? this.props.img.name : '';
 		var isDisplayIcon = { display: (!this.props.img || this.props.img.name.trim() == "") ? "none" : "inline-block" }
 		return (
-			<form ecntype="multipart/form-data" method="POST" onSubmit={this.handleSubmit}>
-				<div className="input-group">
-				   	<div tabIndex="-1" className="form-control file-caption  kv-fileinput-caption">
-				   		<span title={imgName} className="file-caption-ellipsis">…</span>
-				   		<div title={imgName} className="file-caption-name" style={isDisplayIcon}>
-				   			<span className="glyphicon glyphicon-file kv-caption-icon"> {imgName}</span>
-				   		</div>
-					</div>
-				   	<div className="input-group-btn">
-				    	<button type="button" title="Clear selected files" className="btn btn-default fileinput-remove fileinput-remove-button" style={isDisplayIcon} onClick={this.handleRemove}>
-				    		<i className="glyphicon glyphicon-trash"></i> 
-				    	</button>
-				       	<div style={{"overflow": "hidden"}} className="btn btn-primary btn-file">
-				       		<i className="glyphicon glyphicon-picture"></i>
-				       		<span> &nbsp; … </span>
-				       		<input ref={"test"} accept="image/*" className="file" type="file" onChange={this.handleChange} title="Добавить изображение"/>
-				       	</div>
-				   	</div>
+			<div className="input-group">
+			   	<div tabIndex="-1" className="form-control file-caption  kv-fileinput-caption">
+			   		<span title={imgName} className="file-caption-ellipsis">…</span>
+			   		<div title={imgName} className="file-caption-name" style={isDisplayIcon}>
+			   			<span className="glyphicon glyphicon-file kv-caption-icon"> {imgName}</span>
+			   		</div>
 				</div>
-			</form>
+			   	<div className="input-group-btn">
+			    	<button type="button" title="Clear selected files" className="btn btn-default fileinput-remove fileinput-remove-button" style={isDisplayIcon} onClick={this.handleRemove}>
+			    		<i className="glyphicon glyphicon-trash"></i> 
+			    	</button>
+			       	<div style={{"overflow": "hidden"}} className="btn btn-primary btn-file">
+			       		<i className="glyphicon glyphicon-picture"></i>
+			       		<span> &nbsp; … </span>
+
+			       		<form action="/async-upload" method="post" enctype="multipart/form-data" target="iframe-name" onSubmit={this.handleSubmitFile}>
+			       			<input accept="image/*" name="file_name" className="file" type="file" onChange={this.handleChange} title="Добавить изображение"/>
+						</form>
+						<iframe name="iframe-name" src="#" style={{"display":"none"}} onLoad={this.onLoadFrame}></iframe>
+			       	</div>
+			   	</div>
+			</div>
 		);	
 	}
 });
