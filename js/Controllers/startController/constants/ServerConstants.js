cd40de0e1a933a97b6ec04a585df0a35a496aa4e
Explomat <cabehok@inbox.ru>
2015-07-14 10:23:44 +0300

    Fix upload question image error

--- js/Controllers/startController/constants/ServerConstants.js
+++ js/Controllers/startController/constants/ServerConstants.js
@@ -2,8 +2,13 @@ var keyMirror = require('react/lib/keyMirror');
 
 module.exports = keyMirror({
 	RECEIVE_DATA: null,
-	UPLOADED_IMAGE: null,
-	UPLOADED_ERROR_IMAGE: null,
-	REMOVE_IMAGE: null,
-	REMOVE_ERROR_IMAGE: null
+	UPLOADED_ANSWER_IMAGE: null,
+	UPLOADED_ANSWER_ERROR_IMAGE: null,
+	REMOVE_ANSWER_IMAGE: null,
+	REMOVE_ANSWER_ERROR_IMAGE: null,
+
+	UPLOADED_QUESTION_IMAGE: null,
+	UPLOADED_QUESTION_ERROR_IMAGE: null,
+	REMOVE_QUESTION_IMAGE: null,
+	REMOVE_QUESTION_ERROR_IMAGE: null
 });
