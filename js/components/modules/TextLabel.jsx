a49772b0b27a8f7389422880b07ce210ecb99d75
Explomat <cabehok@inbox.ru>
2015-10-20 21:57:02 +0300

    Some styles

--- js/components/modules/TextLabel.jsx
+++ js/components/modules/TextLabel.jsx
@@ -48,25 +48,25 @@ var TextView = React.createClass({
 	mixins: [TextBase],
 
 	handleAddtranslate: function(e){
-		if (!e.target.classList.contains('input_translate')){
-			e.target.classList.add('input_translate');
+		if (!e.target.classList.contains('input-box__label_translate')){
+			e.target.classList.add('input-box__label_translate');
 			this.refs.inpt.getDOMNode().focus();
 		}
 	},
 
 	handleDetranslate: function(e){
-		this.refs.lbl.getDOMNode().classList.remove('input_translate');
-		this.refs.lbl.getDOMNode().classList.add('input_detranslate');
+		this.refs.lbl.getDOMNode().classList.remove('input-box__label_translate');
+		this.refs.lbl.getDOMNode().classList.add('input-box__label_detranslate');
 	},
 
 	render:function() {
-		var isNotEmptyClass = this.state.value === '' ? '' : 'input_not-empty';
+		var isNotEmptyClass = this.state.value === '' ? '' : 'input-box__input_not-empty';
 		var isValidClass = !this.props.isValid(this.state.value) ? this.validClass : ''
 		
 		return (
-			<div className="input_label-box" tabIndex={1} onBlur={this.handleDetranslate}>
-				<input ref="inpt" className={"input_input " + isNotEmptyClass + " " + isValidClass} type="text" value={this.state.value} onChange={this.handleChange} onBlur={this.handleBlur}/>
-                <label ref="lbl" onClick={this.handleAddtranslate} className="input_label">{this.props.placeholder}</label>
+			<div className="input-box" tabIndex={1} onBlur={this.handleDetranslate}>
+				<input ref="inpt" className={"input-box__input " + isNotEmptyClass + " " + isValidClass} type="text" value={this.state.value} onChange={this.handleChange} onBlur={this.handleBlur}/>
+                <label ref="lbl" onClick={this.handleAddtranslate} className="input-box__label">{this.props.placeholder}</label>
 			</div>
 		);
 	}
